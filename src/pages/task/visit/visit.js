78e655a20e3aec31a1d319470334aef40e2b25bc
Donald <tangdezou@qq.com>
2018-10-08 15:03:46 +0800

    拜访地理位置限制

--- src/pages/task/visit/visit.js
+++ src/pages/task/visit/visit.js
@@ -1,14 +1,14 @@
 let app = getApp();
 Page({
   data: {
-    id:'', 
-    vgDetail:{},
+    id: '',
+    vgDetail: {},
     isGetPosition: false,
     longitude: 0,
     latitude: 0
   },
   onLoad(query) {
-    this.setData({id: query.id });
+    this.setData({ id: query.id });
     //this.getVisitGrowerDetail();
     // 页面加载
     console.info(`visit Page onLoad with query: ${JSON.stringify(query)}`);
@@ -18,7 +18,7 @@ Page({
     // 页面显示
     this.getVisitGrowerDetail();
   },
-  getVisitGrowerDetail(){
+  getVisitGrowerDetail() {
     dd.showLoading();
     dd.httpRequest({
       url: app.globalData.host + 'api/services/app/ScheduleTask/GetDingDingVisitGrowerDetailAsync',
@@ -30,7 +30,7 @@ Page({
       success: (res) => {
         //console.info(`schedule: ${JSON.stringify(res.data.result)}`);
         this.setData({ vgDetail: res.data.result });
-        if (res.data.result.growerInfo.longitude && res.data.result.growerInfo.latitude){
+        if (res.data.result.growerInfo.longitude && res.data.result.growerInfo.latitude) {
           this.setData({ isGetPosition: true, longitude: res.data.result.growerInfo.longitude, latitude: res.data.result.growerInfo.latitude });
         }
       },
@@ -43,12 +43,54 @@ Page({
       }
     });
   },
-  goVisit(){
-    dd.navigateTo({
-      url: "../go-visit/go-visit?id=" + this.data.id,
+  goVisit() {
+    //验证
+    var that = this;
+    dd.showLoading();
+    dd.getLocation({
+      success(res) {
+        /*const postjson = {
+          id: that.data.vgDetail.growerInfo.id,
+          longitude: res.longitude,
+          latitude: res.latitude
+        };*/
+        dd.httpRequest({
+          url: app.globalData.host + 'api/services/app/VisitRecord/ValidateLocationAsync?lat=' + res.latitude + '&lon=' + res.longitude 
+            + '&latGrower=' + that.data.latitude + '&lonGrower=' + that.data.longitude,
+          method: 'Post',
+          headers: { 'Content-Type': 'application/json;charset=UTF-8', "Accept": 'application/json' },
+          data: {},//JSON.stringify(postjson),
+          dataType: 'json',
+          success: (res) => {
+            console.info(res.data.result);
+            var result = res.data.result;
+            if (result.code == 0) {
+              //dd.alert({ content: result.msg });
+              dd.navigateTo({
+                url: "../go-visit/go-visit?id=" + that.data.id,
+              });
+            } else {
+              dd.alert({ content: result.msg });
+            }
+          },
+          fail: function(res) {
+            dd.alert({ content: '验证位置异常' });
+            console.info(res);
+          },
+          complete: function(res) {
+            dd.hideLoading();
+            //dd.alert({ content: 'complete' });
+          }
+        });
+      },
+      fail() {
+        dd.hideLoading();
+        dd.alert({ title: '定位失败' });
+      },
     });
+   
   },
-  getPosition(){
+  getPosition() {
     var that = this;
     dd.showLoading();
     dd.getLocation({
@@ -64,13 +106,13 @@ Page({
         };*/
         dd.confirm({
           title: '当前地理位置为',
-          content: reslocation +'（注：只能采集一次）',
+          content: reslocation + '（注：只能采集一次）',
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           success({ confirm }) {
             //console.log(`用户点击了 ${confirm ? '「确定」' : '「取消」'}`);
             console.log(confirm);
-            if (confirm){
+            if (confirm) {
               dd.showLoading();
               dd.httpRequest({
                 url: app.globalData.host + 'api/services/app/Grower/SavePositionAsync?id=' + that.data.vgDetail.growerInfo.id + '&longitude=' + res.longitude + '&latitude=' + res.latitude,
@@ -78,12 +120,12 @@ Page({
                 headers: { 'Content-Type': 'application/json;charset=UTF-8', "Accept": 'application/json' },
                 data: {},//JSON.stringify(postjson),
                 dataType: 'json',
-                success: (res) => {
-                  //console.info(res.data.result);
-                  var result = res.data.result;
+                success: (res2) => {
+                  console.info(res2.data.result);
+                  var result = res2.data.result;
                   if (result.code == 0) {
                     dd.alert({ content: result.msg });
-                    that.setData({ isGetPosition: true, longitude: res.longitude, latitude: res.latitude});
+                    that.setData({ isGetPosition: true, longitude: result.data.lon, latitude: result.data.lat });
                   } else {
                     dd.alert({ content: result.msg });
                   }
@@ -107,8 +149,8 @@ Page({
             console.log('complete');
           },
         });
-          //longitude: res.longitude,
-          //latitude: res.latitude
+        //longitude: res.longitude,
+        //latitude: res.latitude
       },
       fail() {
         dd.hideLoading();
